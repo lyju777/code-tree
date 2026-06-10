b83a7b20edd2f7076cd33c362f9071f7419245b9
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-06-10 22:22:06 +0900

    [Passed] 1부터의 합 (63ms, 10MB)

---
+++ trail0/1부터의 합/sum-from-the-one.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim()
+
+let n = input
+let sum = 0
+
+for(let i = 1; i <= 100; i++){
+    sum+=i
+    if(sum >= n){
+        console.log(i)
+        break
+    }
+}
