29e01688ba8b3dac4404e5138fdee466b2768cbb
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-17 16:50:49 +0900

    [Passed] 입력받은 값 교체하기 (62ms, 10MB)

---
+++ trail1/입력받은 값 교체하기/changing-inputs.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+ 
+let a = Number(input[0])
+let b = Number(input[1])
+
+let temp
+
+temp = a
+a = b
+b = temp
+
+console.log(`${a} ${b}`)
