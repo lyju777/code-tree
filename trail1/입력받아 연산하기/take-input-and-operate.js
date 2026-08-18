8d1998b51f4370ba8f025e4f445904d7900bef6f
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-18 17:44:12 +0900

    [Passed] 입력받아 연산하기 (62ms, 10MB)

---
+++ trail1/입력받아 연산하기/take-input-and-operate.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+let b = Number(input[1])
+
+console.log(`${a+87}`)
+console.log(`${b%10}`)
