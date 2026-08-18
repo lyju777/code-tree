da674a492c89e4ee18e8978edc1d81190fce0453
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-18 16:25:41 +0900

    [Passed] 정수 세 개 입력받아 출력 (61ms, 11MB)

---
+++ trail1/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
+
+console.log(`${a} ${b} ${c}`)
