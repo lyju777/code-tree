0d794f97f6aad84d3585001c61c17d991a3ec84b
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-19 21:42:59 +0900

    [Passed] 등급 매기기 (65ms, 10MB)

---
+++ trail1/등급 매기기/ranking.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+
+if(N >= 90){
+    console.log(`A`)
+} else if(N >= 80){
+      console.log(`B`)
+}else if(N >= 70){
+      console.log(`C`)
+}else if(N >= 60){
+      console.log(`D`)
+}else {
+    console.log(`F`)
+}
