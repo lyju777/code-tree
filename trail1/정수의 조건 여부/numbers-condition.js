d9ffb0dcd17a9fda3a2e99252e5ec3860e18c06b
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-19 21:16:19 +0900

    [Passed] 정수의 조건 여부 (63ms, 10MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+
+if(a >= 113){
+    console.log(1)
+} else {
+    console.log(0)
+}
