de910ccd48ff13c1502b8f34cb5661e76d07dbd8
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-19 21:34:25 +0900

    [Passed] 시력 검사 2 (63ms, 10MB)

---
+++ trail1/시력 검사 2/eye-test-2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+
+if(a >= 1.0){
+    console.log(`High`)
+} else if(a >= 0.5){
+    console.log(`Middle`)
+} else {
+    console.log(`Low`)
+}
