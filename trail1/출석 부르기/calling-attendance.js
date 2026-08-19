788e12f974234d09485e6a26391643d734a56fda
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-19 21:40:47 +0900

    [Passed] 출석 부르기 (67ms, 10MB)

---
+++ trail1/출석 부르기/calling-attendance.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+
+if(a === 1){
+    console.log(`John`)
+} else if(a === 2){
+    console.log(`Tom`)
+} else if(a === 3){
+    console.log(`Paul`)
+} else {
+    console.log(`Vacancy`)
+}
