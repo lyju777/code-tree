095d134e87c71fae84f8199dbb5240cfa7506846
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-22 17:44:08 +0900

    [Passed] 비교 연산 (72ms, 10MB)

---
+++ trail1/비교 연산/comparison-operator.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let A = Number(input[0])
+let B = Number(input[1])
+
+if(A >= B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+if(A > B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+if(A <= B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+if(A < B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+if(A === B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+
+if(A !== B){
+    console.log(1)
+} else {
+    console.log(0)
+}
+
