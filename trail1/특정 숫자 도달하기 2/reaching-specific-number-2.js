4f99752eb0bb6e97a211fa4187025c202b93a83e
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-23 21:14:49 +0900

    [Passed] 특정 숫자 도달하기 2 (61ms, 10MB)

---
+++ trail1/특정 숫자 도달하기 2/reaching-specific-number-2.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+let res = ''
+
+for(let i = N; i >= 1; i--){
+    res+= i+' '
+}
+console.log(res)
