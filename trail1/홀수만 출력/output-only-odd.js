641c38c760c27c5a39d534339e3cb023bfb37989
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-23 21:06:06 +0900

    [Passed] 홀수만 출력 (80ms, 16MB)

---
+++ trail1/홀수만 출력/output-only-odd.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let A = Number(input[0])
+let B = Number(input[1])
+
+let res = ''
+
+for(let i = A; i <= B; i+=2){
+    res+=i+' '
+}
+console.log(res)
