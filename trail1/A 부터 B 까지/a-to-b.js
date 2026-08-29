aed007a4dbf845173ce3a0478064d197106d59fd
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-29 10:14:14 +0900

    [Passed] A 부터 B 까지 (66ms, 10MB)

---
+++ trail1/A 부터 B 까지/a-to-b.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let A = Number(input[0])
+let B = Number(input[1])
+
+let res = ''
+
+while(A <= B){
+    res += A+' '
+    if(A%2 !== 0){
+        A*=2
+    } else {
+        A+=3
+    }
+}
+console.log(res)
