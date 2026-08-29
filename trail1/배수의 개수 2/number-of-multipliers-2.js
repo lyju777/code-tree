14911feff15c07e1589c777881453d6ad29206f7
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-29 15:26:20 +0900

    [Passed] 배수의 개수 2 (72ms, 10MB)

---
+++ trail1/배수의 개수 2/number-of-multipliers-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let cnt = 0
+
+for(let i = 0; i < 10; i++){
+  if(Number(input[i])%2 !== 0){
+    cnt++
+  }
+}
+console.log(cnt)
