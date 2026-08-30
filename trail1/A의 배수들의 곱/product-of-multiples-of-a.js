8f8ec59a06789fb41ea84878ebdc58b3d3bad3b2
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-08-30 17:20:33 +0900

    [Passed] A의 배수들의 곱 (65ms, 10MB)

---
+++ trail1/A의 배수들의 곱/product-of-multiples-of-a.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let A = Number(input[0])
+let B = Number(input[1])
+
+let prod = 1
+
+for(let i = 1; i <= B; i++){
+    if(i%A === 0){
+        prod *= i
+    }
+}
+console.log(prod)
