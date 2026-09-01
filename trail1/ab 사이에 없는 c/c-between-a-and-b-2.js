b0968e02de55cf97adf6bb88c0eb2b4008424cf8
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-01 21:40:27 +0900

    [Passed] ab 사이에 없는 c (63ms, 10MB)

---
+++ trail1/ab 사이에 없는 c/c-between-a-and-b-2.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
+
+let bool = false
+
+for(let i = a; i <= b; i++){
+    if(i%c === 0){
+        bool = true
+        break;
+    }   
+}
+if(bool === true){
+    console.log(`NO`)
+} else {
+    console.log(`YES`)
+}
