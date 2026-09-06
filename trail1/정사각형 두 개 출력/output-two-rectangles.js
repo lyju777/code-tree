0e32f059977d5611fbf00462cbfb29598e7134c9
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-06 15:10:14 +0900

    [Passed] 정사각형 두 개 출력 (67ms, 10MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+let res = ''
+
+for(let i = 0; i < N; i++){
+    res = ''
+    for(let j = 0; j < N; j++){
+        res+='*'
+    }
+    console.log(res)
+}
+
+console.log()
+
+
+for(let i = 0; i < N; i++){
+    res = ''
+    for(let j = 0; j < N; j++){
+        res+='*'
+    }
+    console.log(res)
+}
