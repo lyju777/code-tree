566587ff33b327cf7d69984a5ec2a8a9d1f08f6e
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-07 22:00:30 +0900

    [Passed] 별표 출력하기 4 (64ms, 10MB)

---
+++ trail1/별표 출력하기 4/print-star-4.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+
+let res = ''
+
+for(let i = 0; i < N; i++){
+    res = ''
+    for(let j = 0; j < N-i; j++){
+        res += '* '
+    }
+    console.log(res)
+}
+
+for(let i = 1; i < N; i++){
+    res = ''
+    for(let j = 0; j < i+1; j++){
+        res += '* '
+    }
+    console.log(res)
+}
