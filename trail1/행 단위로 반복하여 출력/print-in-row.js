01d299580859c8894ab78be25a6ff78d078a8c6c
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-12 21:00:03 +0900

    [Passed] 행 단위로 반복하여 출력 (64ms, 10MB)

---
+++ trail1/행 단위로 반복하여 출력/print-in-row.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+
+let res = ''
+
+for(let i = 0; i <N; i++){
+    res = ''
+    for(let j = 0; j < N; j++){
+        res += j+1
+    }
+    console.log(res)
+}
