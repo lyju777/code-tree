0313975c16e83c46df4f55701a5225bb123145b6
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-13 14:47:30 +0900

    [Passed] 정사각형 출력 (65ms, 10MB)

---
+++ trail1/정사각형 출력/print-square.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+
+let res = ''
+let cnt = 1
+
+for(let i = 0 ; i < N; i++){
+    res = ''
+    for(let j = 0; j < N; j++){
+        res += `${cnt++} `
+    }
+    console.log(res)
+}
