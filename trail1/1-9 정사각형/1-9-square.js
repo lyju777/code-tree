29f64a0181abb05064e3a7cc286aaa3696d89e03
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-13 14:52:21 +0900

    [Passed] 1-9 정사각형 (66ms, 10MB)

---
+++ trail1/1-9 정사각형/1-9-square.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+let cnt = 1
+let res = ''
+
+for(let i = 0; i < N; i++){
+    res = ''
+    for(let j = 0; j < N; j++){
+        res += `${cnt++}`
+        if(cnt === 10){
+            cnt = 1
+        }
+    }
+    console.log(res)
+}
