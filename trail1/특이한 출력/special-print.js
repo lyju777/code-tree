e3d5cf8486c2efd9960c28e4618084303252ca67
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-19 11:04:57 +0900

    [Passed] 특이한 출력 (69ms, 10MB)

---
+++ trail1/특이한 출력/special-print.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+let res = ''
+
+for(let i = 1; i <= N; i++){
+    for(let j = 1; j <= N; j++){
+        if((i+j)%4 === 0){
+            res += `(${i}, ${j})\n`
+        } else {
+            res += `(${i}, ${j}) `
+        }
+    }
+}
+
+console.log(res)
