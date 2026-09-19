bcf46cbea373303828310eb29f7a96e3b8cb9d7e
JEONG HUN LEE <86812065+lyju777@users.noreply.github.com>
2026-09-19 10:56:12 +0900

    [Passed] 일직선 구구단 (64ms, 10MB)

---
+++ trail1/일직선 구구단/one-line-multiple.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(/\s+/)
+
+let N = Number(input[0])
+
+
+for(let i = 1; i <= N; i++){
+    res = ''
+    for(let j = 1; j <= N; j++){
+     res += console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
